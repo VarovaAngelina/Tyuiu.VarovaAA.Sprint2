fd305f197705e21a58766363cb73290d65830ff1
Tyuiu.VarovaAA@outlook.com <FoxHighlight@yandex.ru>
2023-10-03 17:10:40 +0500

    Add project files.

---
+++ Tyuiu.VarovaAA.Sprint2.Task0.V20.Lib/DataService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.VarovaAA.Sprint2.Task0.V20.Lib
+{
+    public class DataService : ISprint2Task0V20
+    {
+        public bool[] GetCompareOperations(int x, int y)
+        {
+            bool[] res = new bool[6];
+
+            res[0] = x == y + 800;
+            res[1] = x != y + 800;
+            res[2] = x < y + 1000;
+            res[3] = x > y + 1000;
+            res[4] = x <= y + 800;
+            res[5] = x >= y + 1000;
+
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.VarovaAA.Sprint2.Task0.V20.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.VarovaAA.Sprint2.Task0.V20.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.VarovaAA.Sprint2.Task0.V20.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ed2103aa-3333-444c-82a9-34a4eadc427c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
