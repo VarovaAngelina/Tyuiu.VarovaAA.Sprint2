103983e3098f345e66d26f14787e431c081e8beb
Tyuiu.VarovaAA@outlook.com <FoxHighlight@yandex.ru>
2023-10-08 12:04:03 +0500

    Task2

---
+++ Tyuiu.VarovaAA.Sprint2.Task2.V15.Lib/DataService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.VarovaAA.Sprint2.Task2.V15.Lib
+{
+    public class DataService : ISprint2Task2V15
+    {
+        public bool CheckDotInShadedArea(int x, int y)
+        {
+            bool res;
+
+            if (((((x >= 3) && (x <= 5)) || ((x >= 9) && (x <= 12))) && ((y >= 3) && (y <= 4))) ||
+                (((x >= 2) && (x <= 12)) && (y == 5)) ||
+                (((x >= 2) && (x <= 13)) && (y == 6)) ||
+                (((x >= 3) && (x <= 13)) && (y == 7)) ||
+                (((x >= 6) && (x <= 13)) && (y == 8)) ||
+                ((((x >= 5) && (x <= 6)) || ((x >= 11) && (x <= 12))) && ((y >= 9) && (y <= 10))) ||
+                (((x == 6) || ((x >= 11) && (x <= 12))) && (y == 11)) ||
+                ((((x >= 4) && (x <= 6)) || ((x >= 12) && (x <= 13))) && (y == 12)) ||
+                (((x >= 2) && (x <= 3)) && (y == 13)))
+            {
+                res = true;
+            }
+            else
+            {
+                res = false;
+            }
+
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.VarovaAA.Sprint2.Task2.V15.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.VarovaAA.Sprint2.Task2.V15.Lib;
+namespace Tyuiu.VarovaAA.Sprint2.Task2.V15.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCheckDotInShadedArea()
+        {
+            DataService ds = new DataService();
+            int x = 10;
+            int y = 8;
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+            bool wait = true;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
