8dd1c9f87c552d4ee71062081060809ac3d67460
Tyuiu.VarovaAA@outlook.com <FoxHighlight@yandex.ru>
2023-10-15 20:54:02 +0500

    Task7

---
+++ Tyuiu.VarovaAA.Sprint2.Task7.V1.Lib/DataService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.VarovaAA.Sprint2.Task7.V1.Lib
+{
+    public class DataService : ISprint2Task7V1
+    {
+        public bool CheckDotInShadedArea(double x, double y)
+        {
+            bool res;
+            if (((y >= x) && (y >= -x)) && (((x >= -Math.Sqrt(2) / 2) && (x <= Math.Sqrt(2) / 2)) && ((y >= 0) && (y <= 1))))
+            {
+                res = true;
+            }
+            else
+            {
+                res = false;
+            }
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.VarovaAA.Sprint2.Task7.V1.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.VarovaAA.Sprint2.Task7.V1.Lib;
+namespace Tyuiu.VarovaAA.Sprint2.Task7.V1.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCheckDotInShadedArea()
+        {
+            DataService ds = new DataService();
+
+            double x = 0.0;
+            double y = 0.0;
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+            bool wait = true;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
